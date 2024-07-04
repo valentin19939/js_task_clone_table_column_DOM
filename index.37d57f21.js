5e8406f514f2304e4136c7ddc41f864ad8c18c1f
valentyn19939 <valik.hai1993@gmail.com>
2024-07-04 17:57:33 +0200

    make build

---
+++ index.37d57f21.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("table")[0].querySelectorAll("tr").forEach(function(e){var t=e.children;if(t.length>1){var l=t[1].cloneNode(!0);e.insertBefore(l,t[t.length-1])}});
+//# sourceMappingURL=index.37d57f21.js.map
